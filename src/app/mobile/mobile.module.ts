00565a80bf759a773660999667e9007a18903b28
dRiFT <drift@breakpirates.com>
2017-12-30 13:31:16 +0000

    Added mobile page content

--- src/app/mobile/mobile.module.ts
+++ src/app/mobile/mobile.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { MobileComponent } from './mobile.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [MobileComponent]
 })
