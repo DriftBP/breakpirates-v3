da09862a3d09104fa6ac76c5ab6c13b322085ec8
dRiFT <drift@breakpirates.com>
2018-12-27 22:30:07 +0000

    Schedule module can now be lazy loaded

--- src/app/schedule/schedule.module.ts
+++ src/app/schedule/schedule.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 import { ScheduleComponent } from './schedule.component';
 import { ShowSummaryComponent } from './show-summary/show-summary.component';
+import { ScheduleRoutingModule } from './schedule-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    ScheduleRoutingModule
   ],
   declarations: [ScheduleComponent, ShowSummaryComponent],
   exports: [
