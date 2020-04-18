1effa2bdfe901d690b2d404f112f0e70057554b9
dRiFT <drift@breakpirates.com>
2020-04-18 15:35:22 +0100

    Updated Bootstrap and NGX-Bootstrap to latest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -16,6 +17,7 @@ describe('AppComponent', () => {
       imports: [
         RouterTestingModule,
         HttpClientModule,
+        BrowserAnimationsModule,
         TuneInModule,
         ScheduleModule,
         SharedModule
