e6ed186ae09d6aef6e2d97c5c8a5d561c7ed4fbf
dRiFT <drift@breakpirates.com>
2020-04-20 17:29:01 +0100

    Fixed test

--- src/app/schedule/schedule.component.spec.ts
+++ src/app/schedule/schedule.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ScheduleComponent } from './schedule.component';
+import { ScheduleService } from './schedule.service';
+import { MapToArrayPipe } from '../shared/pipes/map-to-array.pipe';
 
 describe('ScheduleComponent', () => {
   let component: ScheduleComponent;
@@ -8,7 +11,14 @@ describe('ScheduleComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ScheduleComponent ]
+      providers: [ScheduleService],
+      declarations: [
+        ScheduleComponent,
+        MapToArrayPipe
+      ],
+      imports: [
+        HttpClientModule
+      ]
     })
     .compileComponents();
   }));
