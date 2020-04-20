8cffbb9f0b4be69133bbd0a045edbedb38204845
dRiFT <drift@breakpirates.com>
2020-04-20 11:32:59 +0100

    Multi-slot content was causing build and test issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -8,8 +8,6 @@ import { AppRoutingModule } from './/app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { ScheduleModule } from './schedule/schedule.module';
 import { NewsService } from './news/news.service';
-import { ScheduleService } from './schedule/schedule.service';
-import { TuneInService } from './tune-in/tune-in.service';
 import { TuneInModule } from './tune-in/tune-in.module';
 
 @NgModule({
@@ -28,6 +26,7 @@ import { TuneInModule } from './tune-in/tune-in.module';
   providers: [
     NewsService
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
