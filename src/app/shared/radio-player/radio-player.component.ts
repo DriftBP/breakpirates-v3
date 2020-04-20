77fb56f8bc8f4028f1bd5c33429294222dfebb91
dRiFT <drift@breakpirates.com>
2020-04-20 11:46:53 +0100

    Added audio player

---
+++ src/app/shared/radio-player/radio-player.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+import { AppSettings } from '../../appSettings';
+
+@Component({
+  selector: 'app-radio-player',
+  templateUrl: './radio-player.component.html',
+  styleUrls: ['./radio-player.component.scss']
+})
+export class RadioPlayerComponent {
+  tuneInUrl: string;
+
+  constructor() {
+    this.tuneInUrl = AppSettings.TUNE_IN_URL_PRIMARY;
+  }
+
+}
