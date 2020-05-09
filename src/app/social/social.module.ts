274b230a36acd0ae9b9e32fb227d42485f7decc1
dRiFT <drift@breakpirates.com>
2020-05-09 17:13:32 +0100

    Added chat room

--- src/app/social/social.module.ts
+++ src/app/social/social.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SocialComponent } from './social.component';
+import { ChatComponent } from './chat/chat.component';
+import { SocialRoutingModule } from './social-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SocialRoutingModule,
+    SharedModule
   ],
-  declarations: [SocialComponent]
+  declarations: [
+    SocialComponent,
+    ChatComponent
+  ]
 })
 export class SocialModule { }
