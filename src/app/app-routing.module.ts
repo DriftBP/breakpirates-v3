78c822650ab4efcf93733d5baf860d849ece30d8
dRiFT <drift@breakpirates.com>
2020-05-15 22:31:12 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
 
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
