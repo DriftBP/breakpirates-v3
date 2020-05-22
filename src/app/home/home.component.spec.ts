8923ae34860cb3a2451851d1c14a22d9966d0eec
dRiFT <drift@breakpirates.com>
2020-05-22 15:30:40 +0100

    fixed test

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,14 +1,29 @@
 import { async } from '@angular/core/testing';
 import { Shallow } from 'shallow-render';
+import { of } from 'rxjs';
 
 import { HomeComponent } from './home.component';
 import { HomeModule } from './home.module';
+import { ScheduleService } from '../shared/services/schedule.service';
+import { Show } from '../schedule/show';
+
+const mockShow: Show = {
+  id: 1,
+  title: 'title',
+  start_time: '00:00',
+  end_time: '01:00',
+  day_id: 1,
+  description: 'descripion'
+};
 
 describe('HomeComponent', () => {
   let shallow: Shallow<HomeComponent>;
 
   beforeEach(async(() => {
-    shallow = new Shallow(HomeComponent, HomeModule);
+    shallow = new Shallow(HomeComponent, HomeModule)
+      .mock(ScheduleService, {
+        shows: () => of([ mockShow ]),
+      });
   }));
 
   it('should create', async () => {
