74f6634868644a8ca920fbd88cff6fb68ab4ebc5
dRiFT <drift@breakpirates.com>
2020-05-23 20:14:33 +0100

    Switched social

--- src/app/shared/breadcrumb/breadcrumb-config.ts
+++ src/app/shared/breadcrumb/breadcrumb-config.ts
@@ -8,6 +8,10 @@ const scheduleConfig: BreadcrumbConfigItem = { name: 'SCHEDULE.TITLE' };
 export const scheduleConfigInactive = { ...scheduleConfig, routerLink: '/schedule' };
 export const scheduleConfigActive = { ...scheduleConfig, isActive: true };
 
+const socialConfig: BreadcrumbConfigItem = { name: 'SOCIAL.TITLE' };
+export const socialConfigInactive = { ...socialConfig, routerLink: '/social' };
+export const socialConfigActive = { ...socialConfig, isActive: true };
+
 const toolsConfig: BreadcrumbConfigItem = { name: 'TOOLS.TITLE' };
 export const toolsConfigInactive = { ...toolsConfig, routerLink: '/tools' };
 export const toolsConfigActive = { ...toolsConfig, isActive: true };
