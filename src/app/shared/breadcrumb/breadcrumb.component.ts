b34df4a51089a95a3b521f923d3e3302471636fe
dRiFT <drift@breakpirates.com>
2020-05-24 17:10:52 +0100

    Updated title based on breadcrumb

--- src/app/shared/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/breadcrumb/breadcrumb.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnChanges } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
+import { find } from 'lodash';
 
 import { BreadcrumbConfigItem } from './breadcrumb-config-item';
 import { homeConfigActive, homeConfigInactive } from './breadcrumb-config';
@@ -15,11 +18,28 @@ export class BreadcrumbComponent implements OnChanges {
   enabled = AppSettings.ENABLE_BREADCRUMB;
   configItems: BreadcrumbConfigItem[];
 
+  constructor(
+    private titleService: Title,
+    private translateService: TranslateService
+  ) {}
+
   ngOnChanges() {
     if (this.isHome) {
       this.configItems = [homeConfigActive].concat(this.config);
     } else {
       this.configItems = [homeConfigInactive].concat(this.config);
     }
+
+    // Set page title to the translated value of the last breadcrumb item
+    const activeItem = this.getActiveItem(this.configItems);
+
+    if (activeItem) {
+      this.translateService.get(activeItem.name)
+        .subscribe(t => this.titleService.setTitle(`Break Pirates : ${t} - Live Pirate Style Radio`));
+    }
+  }
+
+  private getActiveItem(configItems: BreadcrumbConfigItem[]): BreadcrumbConfigItem {
+    return find(configItems, (i: BreadcrumbConfigItem) => i.isActive);
   }
 }
