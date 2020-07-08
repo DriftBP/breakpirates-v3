6ce6eae0054ad6123c9a1c4fa8097b85a644ba0f
dRiFT <drift@breakpirates.com>
2020-07-08 17:56:46 +0100

    Removed last use of Lodash

--- src/app/shared/pipes/sort-by.pipe.ts
+++ src/app/shared/pipes/sort-by.pipe.ts
@@ -3,7 +3,6 @@
  *ngFor="let c of arrayOfObjects | sortBy:'asc':'propertyName'"
 */
 import { Pipe, PipeTransform } from '@angular/core';
-import orderBy from 'lodash/orderBy';
 
 @Pipe({
   name: 'sortBy'
@@ -11,15 +10,30 @@ import orderBy from 'lodash/orderBy';
 export class SortByPipe implements PipeTransform {
 
   public transform(value: any[], order = '', column: string = ''): any[] {
-    if (!value || order === '' || !order) { return value; } // no array
-    if (value.length <= 1) { return value; } // array with only one item
-    if (!column || column === '') {
-      if (order === 'asc') {
-        return value.sort();
-      } else {
-        return value.sort().reverse();
+    return value.sort(this.compareValues(column, order));
+  }
+
+  private compareValues(key: string, order = 'asc') {
+    return function innerSort(a, b) {
+      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+        // property doesn't exist on either object
+        return 0;
       }
-    } // sort 1d array
-    return orderBy(value, [column], [order]);
+
+      const varA = (typeof a[key] === 'string')
+        ? a[key].toUpperCase() : a[key];
+      const varB = (typeof b[key] === 'string')
+        ? b[key].toUpperCase() : b[key];
+
+      let comparison = 0;
+      if (varA > varB) {
+        comparison = 1;
+      } else if (varA < varB) {
+        comparison = -1;
+      }
+      return (
+        (order === 'desc') ? (comparison * -1) : comparison
+      );
+    };
   }
 }
