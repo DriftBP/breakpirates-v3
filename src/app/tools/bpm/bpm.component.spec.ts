54c3417d03088fab4d6c6d3ebaf7de806e3447b4
dRiFT <drift@breakpirates.com>
2020-09-08 19:06:51 +0100

    Updated component to be more testable and added tests

--- src/app/tools/bpm/bpm.component.spec.ts
+++ src/app/tools/bpm/bpm.component.spec.ts
@@ -1,8 +1,18 @@
 import { waitForAsync } from '@angular/core/testing';
 import { Shallow } from 'shallow-render';
+import moment from 'moment';
 
-import { BpmComponent } from './bpm.component';
+import { BpmComponent, DataCollectionStatus } from './bpm.component';
 import { ToolsModule } from '../tools.module';
+
+const maxDataPoints = 20;
+const emptyDataPoints = [];
+const insufficientDataPoints = new Array(maxDataPoints - 1).fill({
+  time: moment()
+}, 0, maxDataPoints);
+const fullDataPoints = new Array(maxDataPoints).fill({
+  time: moment()
+}, 0, maxDataPoints);
 
 describe('BpmComponent', () => {
   let shallow: Shallow<BpmComponent>;
@@ -15,5 +25,33 @@ describe('BpmComponent', () => {
     const { element } = await shallow.render();
 
     expect(element.nativeElement).toBeTruthy();
+  });
+
+  it('should recognise no data points', async () => {
+    const { instance } = await shallow.render();
+
+    const result = instance['isEmpty'](emptyDataPoints);
+
+    expect(result).toBeTruthy();
+  });
+
+  it('should recognise required number of data points', async () => {
+    const { instance } = await shallow.render();
+
+    const result = instance['isFull'](fullDataPoints);
+
+    expect(result).toBeTruthy();
+  });
+
+  it('should return relevant status', async () => {
+    const { instance } = await shallow.render();
+
+    const emptyStatus = instance['getStatus'](emptyDataPoints);
+    const insufficientStatus = instance['getStatus'](insufficientDataPoints);
+    const fullStatus = instance['getStatus'](fullDataPoints);
+
+    expect(emptyStatus).toEqual(DataCollectionStatus.Empty);
+    expect(insufficientStatus).toEqual(DataCollectionStatus.Insufficient);
+    expect(fullStatus).toEqual(DataCollectionStatus.Full);
   });
 });
