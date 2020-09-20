091545cf648a147a3bca2cfb61e12f19659a56fc
dRiFT <drift@breakpirates.com>
2020-09-20 18:32:05 +0100

    Fixed test

--- src/app/shared/directives/image-click.directive.spec.ts
+++ src/app/shared/directives/image-click.directive.spec.ts
@@ -1,16 +1,28 @@
+import { ElementRef } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 
+import { DialogService } from '../services/dialog.service';
 import { ImageClickDirective } from './image-click.directive';
+
+class MockDialogService extends DialogService {}
+
+class MockElementRef extends ElementRef {
+  constructor() { super(null); }
+}
 
 describe('ImageClickDirective', () => {
   beforeEach(async () => {
     TestBed.configureTestingModule({
-      declarations: [ImageClickDirective]
+      declarations: [ImageClickDirective],
+      providers: [
+        { provide: DialogService, useClass: MockDialogService },
+        { provide: ElementRef, useClass: MockElementRef }
+      ]
     }).compileComponents();
   });
 
   it('should create an instance', () => {
-    const directive = new ImageClickDirective();
+    const directive = new ImageClickDirective(new MockDialogService(), new MockElementRef());
     expect(directive).toBeTruthy();
   });
 });
