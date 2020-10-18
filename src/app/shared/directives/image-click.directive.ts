0c8f2effeecb6ab0982fa509e07f189611c0595e
dRiFT <drift@breakpirates.com>
2020-10-18 12:06:33 +0100

    Image now displayed at up to natural size

--- src/app/shared/directives/image-click.directive.ts
+++ src/app/shared/directives/image-click.directive.ts
@@ -12,13 +12,13 @@ export class ImageClickDirective {
       e.preventDefault();
       e.stopPropagation();
       const src = this.element.nativeElement['src'];
-      const width = this.element.nativeElement['width'];
-      const height = this.element.nativeElement['height'];
+      const width = this.element.nativeElement['naturalWidth'];
+      const height = this.element.nativeElement['naturalHeight'];
       const alt = this.element.nativeElement['alt'];
 
       const dialogConfig: IDialogConfig = {
         title: alt,
-        content: `<img src="${src}" width="${width}" height="${height}" alt="${alt}">`
+        content: `<img src="${src}" width="${width}" height="${height}" alt="${alt}" style="max-width: 100%; height: auto">`
       };
 
       this.dialogService.showDialog(dialogConfig);
