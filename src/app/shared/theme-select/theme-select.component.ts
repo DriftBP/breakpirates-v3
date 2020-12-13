14918be471d1953af9619a9f27a8a9eda1d26180
dRiFT <drift@breakpirates.com>
2020-12-13 13:02:35 +0000

    Optimisations

--- src/app/shared/theme-select/theme-select.component.ts
+++ src/app/shared/theme-select/theme-select.component.ts
@@ -4,13 +4,31 @@ import { ThemeService } from '../services/theme/theme.service';
 import { ThemeSetting } from '../services/theme/theme-setting';
 import { GoogleAnalyticsService } from '../services/google-analytics/google-analytics.service';
 
+interface IThemeOption {
+  setting: ThemeSetting;
+  key: string;
+}
+
 @Component({
   selector: 'bp-theme-select',
   templateUrl: './theme-select.component.html',
   styleUrls: ['./theme-select.component.scss']
 })
 export class ThemeSelectComponent {
-  themeSettings = ThemeSetting;
+  options: IThemeOption[] = [
+    {
+      setting: ThemeSetting.Auto,
+      key: 'FOOTER.AUTO'
+    },
+    {
+      setting: ThemeSetting.Light,
+      key: 'FOOTER.LIGHT'
+    },
+    {
+      setting: ThemeSetting.Dark,
+      key: 'FOOTER.DARK'
+    },
+  ];
 
   constructor(
     private readonly themeService: ThemeService,
