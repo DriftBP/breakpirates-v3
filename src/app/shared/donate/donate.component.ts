8ec4b94a03bc0862a22c20a336aee39a7f79b08f
dRiFT <drift@breakpirates.com>
2020-12-28 19:48:47 +0000

    Track donate button clicks

--- src/app/shared/donate/donate.component.ts
+++ src/app/shared/donate/donate.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
 
+import { GoogleAnalyticsService } from '../services/google-analytics/google-analytics.service';
+
 @Component({
   selector: 'bp-donate',
   templateUrl: './donate.component.html'
 })
-export class DonateComponent {}
+export class DonateComponent {
+  constructor(
+    private readonly googleAnalyticsService: GoogleAnalyticsService
+  ) {}
+
+  onSubmit() {
+    this.googleAnalyticsService.trackEvent('click', 'Donate button', 'Side bar');
+  }
+}
