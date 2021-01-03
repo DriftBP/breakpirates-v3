7c1cb02a1c3b04e7ce3bb27c1f472355d751bc42
dRiFT <drift@breakpirates.com>
2021-01-03 15:05:16 +0000

    Ad refreshing not working, so removing for now

--- src/app/shared/ad-unit/ad-unit.component.ts
+++ src/app/shared/ad-unit/ad-unit.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnDestroy } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { AppSettings } from '../../app-settings';
 
@@ -7,54 +7,10 @@ import { AppSettings } from '../../app-settings';
   templateUrl: './ad-unit.component.html',
   styleUrls: ['./ad-unit.component.scss']
 })
-export class AdUnitComponent implements OnChanges, OnDestroy {
+export class AdUnitComponent {
   @Input() adSlot: number;
   @Input() width: number;
   @Input() height: number;
-  @Input() refreshSecs: number;
-
-  private interval: number;
-  private refreshMs = 0;
-  private refreshDelayMs = 10;
 
   adsenseClient = AppSettings.ADSENSE_CLIENT;
-  displayAd: boolean;
-
-  constructor() {
-    this.showAd();
-  }
-
-  ngOnChanges() {
-    if (this.refreshSecs) {
-      this.refreshMs = this.refreshSecs * 1000;
-
-      if (this.interval) {
-        window.clearInterval(this.interval);
-      }
-
-      this.interval = window.setInterval(this.onRefresh.bind(this), this.refreshMs);
-    }
-  }
-
-  ngOnDestroy() {
-    if (this.interval) {
-      window.clearInterval(this.interval);
-    }
-  }
-
-  private hideAd(): void {
-    this.displayAd = false;
-  }
-
-  private showAd(): void {
-    this.displayAd = true;
-  }
-
-  private onRefresh(): void {
-    this.hideAd();
-
-    window.setTimeout(() => {
-      this.showAd();
-    }, this.refreshDelayMs);
-  }
 }
