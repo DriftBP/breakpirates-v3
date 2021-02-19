7e3fd07182cab8662f414423d47cb58415d01150
DriftBP <33351136+DriftBP@users.noreply.github.com>
2021-02-19 23:59:59 +0000

    Switched test to use shallow-render

--- src/app/schedule/day.service.spec.ts
+++ src/app/schedule/day.service.spec.ts
@@ -1,33 +1,33 @@
-import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { Shallow } from 'shallow-render';
 
 import { DayService } from './day.service';
+import { ScheduleModule } from './schedule.module';
 
 describe('DayService', () => {
+  let shallow: Shallow<DayService>;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [DayService],
-      imports: [
-        HttpClientModule
-      ]
-    });
+    shallow = new Shallow(DayService, ScheduleModule);
   });
 
-  it('should be created', inject([DayService], (service: DayService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should be created', () => {
+    const {instance} = shallow.createService();
+    expect(instance).toBeTruthy();
+  });
 
-  it('should return 7 days', inject([DayService], (service: DayService) => {
-    expect(service.days().length).toEqual(7);
-  }));
+  it('should return 7 days', () => {
+    const {instance} = shallow.createService();
+    expect(instance.days().length).toEqual(7);
+  });
 
-  it('should return day name', inject([DayService], (service: DayService) => {
-    expect(service.dayName(1)).toEqual('Monday');
-    expect(service.dayName(2)).toEqual('Tuesday');
-    expect(service.dayName(3)).toEqual('Wednesday');
-    expect(service.dayName(4)).toEqual('Thursday');
-    expect(service.dayName(5)).toEqual('Friday');
-    expect(service.dayName(6)).toEqual('Saturday');
-    expect(service.dayName(7)).toEqual('Sunday');
-  }));
+  it('should return day name', () => {
+    const {instance} = shallow.createService();
+    expect(instance.dayName(1)).toEqual('Monday');
+    expect(instance.dayName(2)).toEqual('Tuesday');
+    expect(instance.dayName(3)).toEqual('Wednesday');
+    expect(instance.dayName(4)).toEqual('Thursday');
+    expect(instance.dayName(5)).toEqual('Friday');
+    expect(instance.dayName(6)).toEqual('Saturday');
+    expect(instance.dayName(7)).toEqual('Sunday');
+  });
 });
