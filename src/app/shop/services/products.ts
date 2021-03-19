67e3c939a22fc7be096f5f953b03d903764d0871
DriftBP <33351136+DriftBP@users.noreply.github.com>
2021-03-19 18:38:31 +0000

    Removed unused method and empty constructor

---
+++ src/app/shop/services/products.ts
@@ -0,0 +1,52 @@
+import { ProductType } from "./product-type";
+
+export const products = [
+  {
+    Asin: 'B01CRI3UOU', // Sennheiser HD25
+    Type: ProductType.Headphones
+  },
+  {
+    Asin: 'B0069JFBM8', // Sennheiser HD25 earpads
+    Type: ProductType.Headphones
+  },
+  {
+    Asin: 'B076S9VFKV', // Reloop RP7000 MK2
+    Type: ProductType.Turntable
+  },
+  {
+    Asin: 'B00W3T3TOK', // Reloop RMX22i
+    Type: ProductType.Mixer
+  },
+  {
+    Asin: 'B074L92B6W', // Pioneer DJM750 MK2
+    Type: ProductType.Mixer
+  },
+  {
+    Asin: 'B000ML4KT4', // Ortofon Concorde Pro S
+    Type: ProductType.CartStylus
+  },
+  {
+    Asin: 'B00GPHDH6G', // Reloop Concorde Black
+    Type: ProductType.CartStylus
+  },
+  {
+    Asin: 'B00CIVSQJ6', // Serato timecode vinyl
+    Type: ProductType.Dvs
+  },
+  {
+    Asin: 'B01AKAHF52', // Velvet Vinyl Cleaning Brush by SPINCARE
+    Type: ProductType.VinylCare
+  },
+  {
+    Asin: 'B000BFXIVW', // DISCO-ANTISTAT
+    Type: ProductType.VinylCare
+  },
+  {
+    Asin: 'B002GHBYZ0', // Behringer UFO202
+    Type: ProductType.AudioInterface
+  },
+  {
+    Asin: 'B0023BYDHK', // Behringer U-Control UCA222
+    Type: ProductType.AudioInterface
+  }
+];
--- src/app/shop/services/shop.service.ts
+++ src/app/shop/services/shop.service.ts
@@ -2,65 +2,11 @@ import { Injectable } from '@angular/core';
 
 import { Product } from './product';
 import { ProductType } from './product-type';
+import { products } from './products';
 
 @Injectable()
 export class ShopService {
-  private allProducts = [
-    {
-      Asin: 'B01CRI3UOU', // Sennheiser HD25
-      Type: ProductType.Headphones
-    },
-    {
-      Asin: 'B0069JFBM8', // Sennheiser HD25 earpads
-      Type: ProductType.Headphones
-    },
-    {
-      Asin: 'B076S9VFKV', // Reloop RP7000 MK2
-      Type: ProductType.Turntable
-    },
-    {
-      Asin: 'B00W3T3TOK', // Reloop RMX22i
-      Type: ProductType.Mixer
-    },
-    {
-      Asin: 'B074L92B6W', // Pioneer DJM750 MK2
-      Type: ProductType.Mixer
-    },
-    {
-      Asin: 'B000ML4KT4', // Ortofon Concorde Pro S
-      Type: ProductType.CartStylus
-    },
-    {
-      Asin: 'B00GPHDH6G', // Reloop Concorde Black
-      Type: ProductType.CartStylus
-    },
-    {
-      Asin: 'B00CIVSQJ6', // Serato timecode vinyl
-      Type: ProductType.Dvs
-    },
-    {
-      Asin: 'B01AKAHF52', // Velvet Vinyl Cleaning Brush by SPINCARE
-      Type: ProductType.VinylCare
-    },
-    {
-      Asin: 'B000BFXIVW', // DISCO-ANTISTAT
-      Type: ProductType.VinylCare
-    },
-    {
-      Asin: 'B002GHBYZ0', // Behringer UFO202
-      Type: ProductType.AudioInterface
-    },
-    {
-      Asin: 'B0023BYDHK', // Behringer U-Control UCA222
-      Type: ProductType.AudioInterface
-    }
-  ];
-
-  constructor() { }
-
-  getProducts(): Product[] {
-    return this.allProducts;
-  }
+  private allProducts = products;
 
   getProductsByType(type: ProductType): Product[] {
     return this.allProducts.filter(product => product.Type === type);
