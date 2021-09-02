204fe45aadb8b49a64086c91019e60d7f7f8334b
DriftBP <33351136+DriftBP@users.noreply.github.com>
2021-09-02 19:10:25 +0100

    Fixed tests

--- src/app/tools/soundboard/soundboard.component.spec.ts
+++ src/app/tools/soundboard/soundboard.component.spec.ts
@@ -3,12 +3,18 @@ import { Shallow } from 'shallow-render';
 
 import { SoundboardComponent } from './soundboard.component';
 import { SoundboardModule } from './soundboard.module';
+import { SoundboardService } from './soundboard.service';
+
+const mockSoundboardService = {
+  initialise: jest.fn()
+};
 
 describe('SoundboardComponent', () => {
   let shallow: Shallow<SoundboardComponent>;
 
   beforeEach(waitForAsync(() => {
-    shallow = new Shallow(SoundboardComponent, SoundboardModule);
+    shallow = new Shallow(SoundboardComponent, SoundboardModule)
+      .mock(SoundboardService, mockSoundboardService);
   }));
 
   it('should create', async () => {
