a78314d792a0b7207bdae196c0d1b2ab5cc2e98e
DriftBP <33351136+DriftBP@users.noreply.github.com>
2022-12-30 13:37:12 +0000

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 /** @type {import('jest').Config} */
 const config = {
-  preset: "jest-preset-angular",
+  preset: 'jest-preset-angular',
   setupFilesAfterEnv: [
-    "<rootDir>/setupJest.ts"
+    '<rootDir>/setupJest.ts'
   ],
   transformIgnorePatterns: [
     "^.+\\.js$"
-  ]
+  ],
+  globalSetup: 'jest-preset-angular/global-setup',
 };
 
 module.exports = config;
