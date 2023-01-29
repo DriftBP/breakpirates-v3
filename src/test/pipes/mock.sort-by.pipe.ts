c9a5eb2a359840fab3620be13ec3cd85f59fcd14
DriftBP <33351136+DriftBP@users.noreply.github.com>
2023-01-29 15:18:17 +0000

    Fix sort by pipe error: Pipes should implement `PipeTransform`

--- src/test/pipes/mock.sort-by.pipe.ts
+++ src/test/pipes/mock.sort-by.pipe.ts
@@ -1,6 +1,11 @@
-import { Pipe } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'sortBy'
 })
-export class MockSortByPipe {}
+export class MockSortByPipe implements PipeTransform {
+  transform(value: number): number {
+    //Do stuff here, if you want
+    return value;
+  }
+}
