9edae789d5cb99c16035df90b78932bca4e50e67
DriftBP <33351136+DriftBP@users.noreply.github.com>
2023-08-13 13:55:59 +0100

    Added link to standalone player

--- src/app/standalone-player/standalone-player.component.ts
+++ src/app/standalone-player/standalone-player.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+
+import { AppSettings } from '../app-settings';
 
 @Component({
   selector: 'bp-standalone-player',
-  templateUrl: './standalone-player.component.html'
+  templateUrl: './standalone-player.component.html',
+  styleUrls: ['./standalone-player.component.scss']
 })
-export class StandalonePlayerComponent {}
+export class StandalonePlayerComponent implements OnDestroy {
+  assetRoot = AppSettings.ASSET_ROOT;
+
+  constructor() {
+    document.body.className = 'bp-popup';
+  }
+
+  ngOnDestroy(): void {
+    document.body.className = '';
+  }
+}
