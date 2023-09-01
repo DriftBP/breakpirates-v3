f07d705e6b4751a7ab35cd2da112e65b8fedf5f9
DriftBP <33351136+DriftBP@users.noreply.github.com>
2023-09-01 22:30:39 +0100

    Removed unused import

--- src/app/music/resolves/genre-shows.resolve.ts
+++ src/app/music/resolves/genre-shows.resolve.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot } from '@angular/router';
 
-import { Show } from '../../schedule/models/show';
 import { MusicService } from '../services/music.service';
 import { MusicResolvesModule } from './music-resolves.module';
 
