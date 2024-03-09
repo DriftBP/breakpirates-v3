cdd41a546afe28087ea6301e808c47eaf5080a60
DriftBP <33351136+DriftBP@users.noreply.github.com>
2024-03-09 23:04:19 +0000

    Update news-article.component.spec.ts

--- src/app/news/news-article/news-article.component.spec.ts
+++ src/app/news/news-article/news-article.component.spec.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute } from '@angular/router';
 import { NewsArticleComponent } from './news-article.component';
 import { BreadcrumbService } from '../../shared/services/breadcrumb/breadcrumb.service';
 import { MockBreadcrumbService } from '../../../test/services/mock.breadcrumb.service';
+import { mockArticleWithImage, mockArticleWithoutImage } from '../../../test/data/mock.articles';
+import { By } from '@angular/platform-browser';
 
 describe('NewsArticleComponent', () => {
   let component: NewsArticleComponent;
@@ -31,5 +33,28 @@ describe('NewsArticleComponent', () => {
 
   it('should create', async () => {
     expect(component).toBeDefined();
+  });
+
+  describe('News article images', () => {
+    it('should not display an image if the article doesn`t have one defined', async () => {
+      component.article = mockArticleWithoutImage;
+
+      fixture.detectChanges();
+
+      const image = fixture.debugElement.query(By.css('.news-article__image'));
+
+      expect(image).toBeNull();
+    });
+
+    it('should display an image if the article has one defined', async () => {
+      component.article = mockArticleWithImage;
+
+      fixture.detectChanges();
+
+      const image: HTMLImageElement = fixture.debugElement.query(By.css('.news-article__image')).nativeElement;
+
+      expect(image).toBeDefined();
+      expect(image.src).toBeDefined();
+    });
   });
 });
