a2558ad231c830b96af94db39916dba3316c5ba6
DriftBP <33351136+DriftBP@users.noreply.github.com>
2024-03-11 21:37:29 +0000

    Add null check

--- src/app/shared/now-playing/now-playing.component.ts
+++ src/app/shared/now-playing/now-playing.component.ts
@@ -61,7 +61,7 @@ export class NowPlayingComponent implements OnDestroy {
     const url = `http://${hostname}${port}/player`;
     const params = `toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=143`;
 
-    window.open(url, 'player', params).focus();
+    window.open(url, 'player', params)?.focus();
   }
 
   ngOnDestroy() {
