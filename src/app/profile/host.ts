1e3430279343b694a5c5dd74b09f80937d5f147d
DriftBP <33351136+DriftBP@users.noreply.github.com>
2024-06-10 19:29:56 +0100

    Update host.ts

--- src/app/profile/host.ts
+++ src/app/profile/host.ts
@@ -1,9 +1,9 @@
 export interface Host {
-    id: number;
-    name: string;
-    biog: string | null;
-    image: string | null;
-    location: string | null;
-    mixcloud: string | null;
-    twitter: string | null;
+  id: number;
+  name: string;
+  biog: string | null;
+  image: string | null;
+  location: string | null;
+  mixcloud: string | null;
+  twitter: string | null;
 }
