0ce677c034646a2f611e8043240b9ec69ae87c59
DriftBP <33351136+DriftBP@users.noreply.github.com>
2024-06-10 19:24:43 +0100

    Add missing Host properties

--- src/test/data/mock.profiles.ts
+++ src/test/data/mock.profiles.ts
@@ -2,5 +2,10 @@ import { Host } from "../../app/profile/host";
 
 export const mockHost: Host = {
   id: 1,
-  name: 'name'
+  name: 'name',
+  biog: null,
+  image: null,
+  location: null,
+  twitter: null,
+  mixcloud: null
 };
