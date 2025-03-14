4ad7683c65b5fa8c5260a3a82ceae00ca9afab08
DriftBP <33351136+DriftBP@users.noreply.github.com>
2025-03-14 11:14:08 +0000

    Schedule section

--- src/app/schedule/schedule-routing.module.ts
+++ src/app/schedule/schedule-routing.module.ts
@@ -4,24 +4,52 @@ import { Routes, RouterModule } from '@angular/router';
 import { ScheduleComponent } from './schedule.component';
 import { ShowComponent } from './show/show.component';
 import { DayScheduleComponent } from './day-schedule/day-schedule.component';
-import { ScheduleResolve } from './resolves/schedule.resolve';
-import { ShowDetailsResolve } from './resolves/show-details.resolve';
-import { TodaysScheduleResolve } from './resolves/todays-schedule.resolve';
-import { DaysResolve } from './resolves/days.resolve';
-import { ScheduleResolvesModule } from './resolves/schedule-resolves.module';
+import { scheduleResolver } from './resolves/schedule.resolver';
+import { showDetailsResolver } from './resolves/show-details.resolver';
+import { todaysScheduleResolver } from './resolves/todays-schedule.resolver';
+import { daysResolver } from './resolves/days.resolver';
+import { ScheduleResolversModule } from './resolves/schedule-resolvers.module';
 import { validDayGuard } from './guards/valid-day.guard';
 
 const routes: Routes = [
-  { path: '', component: ScheduleComponent, resolve: { days: DaysResolve }, children: [
-    { path: '', component: DayScheduleComponent, resolve: { schedule: TodaysScheduleResolve } },
-    { path: ':day', component: DayScheduleComponent, canActivate: [validDayGuard()], resolve: { schedule: ScheduleResolve } },
-  ] },
-  { path: 'shows/:id', component: ShowComponent, resolve: { show: ShowDetailsResolve } }
+  {
+    path: '',
+    component: ScheduleComponent,
+    resolve: {
+      days: daysResolver
+    },
+    children: [
+      {
+        path: '',
+        component: DayScheduleComponent,
+        resolve: {
+          schedule: todaysScheduleResolver
+        }
+      },
+      {
+        path: ':day',
+        component: DayScheduleComponent,
+        canActivate: [
+          validDayGuard()
+        ],
+        resolve: {
+          schedule: scheduleResolver
+        }
+      },
+    ]
+  },
+  {
+    path: 'shows/:id',
+    component: ShowComponent,
+    resolve: {
+      show: showDetailsResolver
+    }
+  }
 ];
 
 @NgModule({
   imports: [
-    ScheduleResolvesModule,
+    ScheduleResolversModule,
     RouterModule.forChild(routes)
   ],
   exports: [RouterModule]
