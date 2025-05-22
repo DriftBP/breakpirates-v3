b1c2ae6dece2e2437ff3880f1cdfc4cab777250f
DriftBP <33351136+DriftBP@users.noreply.github.com>
2025-05-22 20:23:10 +0100

    Clean up profile module

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,27 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-import { ProfilesComponent } from './profiles.component';
-import { HostDetailsComponent } from './host-details/host-details.component';
-import { SharedModule } from '../shared/shared.module';
 import { ScheduleModule } from '../schedule/schedule.module';
 import { ProfileRoutingModule } from './profile-routing.module';
-import { ProfileButtonComponent } from './profile-button/profile-button.component';
 import { ProfileService } from './services/profile.service';
 
 @NgModule({
   imports: [
-    CommonModule,
-    RouterModule,
     ProfileRoutingModule,
-    SharedModule,
-    ScheduleModule,
-    FontAwesomeModule,
-    HostDetailsComponent,
-    ProfilesComponent,
-    ProfileButtonComponent
+    ScheduleModule
   ],
   providers: [
     ProfileService
