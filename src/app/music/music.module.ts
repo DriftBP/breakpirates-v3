2be1e41548c8f42cee1dec05a736561d004b2110
DriftBP <33351136+DriftBP@users.noreply.github.com>
2025-05-22 21:20:02 +0100

    Music components to standalone

--- src/app/music/music.module.ts
+++ src/app/music/music.module.ts
@@ -1,21 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
-import { MusicComponent } from './music.component';
-import { GenreComponent } from './genre/genre.component';
-import { ScheduleModule } from '../schedule/schedule.module';
 import { MusicRoutingModule } from './music-routing.module';
-import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    RouterModule,
-    SharedModule,
-    MusicRoutingModule,
-    ScheduleModule
-  ],
-  declarations: [MusicComponent, GenreComponent]
+    MusicRoutingModule
+  ]
 })
 export class MusicModule { }
