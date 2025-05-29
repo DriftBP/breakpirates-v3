34f6abdfc508989d4ca316fd789aebd7f13968e1
DriftBP <33351136+DriftBP@users.noreply.github.com>
2025-05-29 21:06:08 +0100

    Remove unused imports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,8 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { AlertModule } from 'ngx-bootstrap/alert';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 export function customHttpLoader(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -16,11 +10,6 @@ export function customHttpLoader(http: HttpClient) {
 
 @NgModule({
   imports: [
-    BsDropdownModule.forRoot(),
-    CollapseModule.forRoot(),
-    AlertModule.forRoot(),
-    CommonModule,
-    RouterModule,
     TranslateModule.forChild({
       loader: {
         provide: TranslateLoader,
@@ -28,9 +17,7 @@ export function customHttpLoader(http: HttpClient) {
         deps: [HttpClient],
       },
       isolate: false
-    }),
-    FontAwesomeModule,
-    NgOptimizedImage
+    })
   ],
   exports: [
     TranslateModule
