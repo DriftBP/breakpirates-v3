e506221e1e080cd05390ffd4a11548cf7fa08521
DriftBP <33351136+DriftBP@users.noreply.github.com>
2025-08-08 16:51:49 +0100

    Use signal for title updates

--- src/app/shared/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/breadcrumb/breadcrumb.component.ts
@@ -1,7 +1,7 @@
 import { Component, Signal, computed, effect, inject } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { TranslatePipe, TranslateService, Translation } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 import { BreadcrumbConfigItem } from './breadcrumb-config-item';
 import { homeConfigActive, homeConfigInactive } from './breadcrumb-config';
@@ -27,6 +27,12 @@ export class BreadcrumbComponent {
   enabled = AppSettings.ENABLE_BREADCRUMB;
   configItems: Signal<BreadcrumbConfigItem[]>;
 
+  readonly translatedTitle = computed(() => {
+    const activeItem = this.getActiveItem(this.configItems());
+    if (!activeItem) return '';
+    return this.translateService.instant(activeItem.name) + ' : Break Pirates - Live Pirate Style Radio';
+  });
+
   constructor() {
     this.configItems = computed(() => {
       var config = this.breadcrumbService.breadcrumb();
@@ -43,11 +49,11 @@ export class BreadcrumbComponent {
     });
 
     effect(() => {
-      // Set page title to the translated value of the last breadcrumb item
-      const activeItem = this.getActiveItem(this.configItems());
-
-      if (activeItem) {
-        this.setTitle(activeItem);
+      const title = this.translatedTitle();
+      if (title) {
+        this.titleService.setTitle(title);
+        this.meta.updateTag({ property: 'og:title', content: title });
+        this.meta.updateTag({ name: 'twitter:title', content: title });
       }
     });
   }
@@ -58,16 +64,5 @@ export class BreadcrumbComponent {
 
   private isHome(config: BreadcrumbConfigItem[]): boolean {
     return config && config.length === 0;
-  }
-
-  private setTitle(activeItem: BreadcrumbConfigItem): void {
-    this.translateService.get(activeItem.name)
-      .subscribe((t: Translation) => {
-        var title = `${t} : Break Pirates - Live Pirate Style Radio`;
-
-        this.titleService.setTitle(title);
-        this.meta.updateTag({ property: 'og:title', content: title });
-        this.meta.updateTag({ name: 'twitter:title', content: title });
-      });
   }
 }
