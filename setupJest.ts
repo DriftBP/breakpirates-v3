45607fd32f32591b6087a06e67eec95f87874996
DriftBP <33351136+DriftBP@users.noreply.github.com>
2025-08-10 14:42:39 +0100

    Add comment

--- setupJest.ts
+++ setupJest.ts
@@ -2,6 +2,7 @@ import { setupZonelessTestEnv } from 'jest-preset-angular/setup-env/zoneless';
 
 setupZonelessTestEnv();
 
+/* fix for IntersectionObserver not being defined in tests */
 Object.defineProperty(global, 'IntersectionObserver', {
   writable: true,
   configurable: true,
