d2f45eb3a8ee9fa15b31cdefd01b4016a9a6b129
DriftBP <33351136+DriftBP@users.noreply.github.com>
2026-01-22 21:49:43 +0000

    Fix tests

--- src/app/music/services/music.service.spec.ts
+++ src/app/music/services/music.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { MusicService } from './music.service';
 
@@ -10,7 +11,8 @@ describe('MusicService', () => {
     TestBed.configureTestingModule({
       providers: [
         MusicService,
-        provideHttpClient(withInterceptorsFromDi())
+        provideHttpClient(),
+        provideHttpClientTesting()
       ]
     });
 
